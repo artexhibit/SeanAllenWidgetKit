55bbce6b5ec4c88853fcc77f6465574fe4564742
artexhibit <vendabal@yandex.ru>
2023-11-19 19:15:11 +0300

    Add large widget, refactor Repository Model

---
+++ RepoWatcher/RepoWatcherWidget/RepoMediumView.swift
@@ -0,0 +1,18 @@
+//
+//  RepoMediumView.swift
+//  RepoWatcherWidgetExtension
+//
+//  Created by Igor Volkov on 19.11.2023.
+//
+
+import SwiftUI
+
+struct RepoMediumView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepoMediumView()
+}

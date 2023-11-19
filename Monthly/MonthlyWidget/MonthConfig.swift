e255cf13407cdc3901aba1a898ffa3b2af95fc93
artexhibit <vendabal@yandex.ru>
2023-11-19 13:38:22 +0300

    adjust code for iOS17, add a special widget view for 01.01

--- Monthly/MonthlyWidget/MonthConfig.swift
+++ Monthly/MonthlyWidget/MonthConfig.swift
@@ -5,89 +5,112 @@ struct MonthConfig {
     let emojiText: String
     let weekDayTextColor: Color
     let dayTextColor: Color
+    let januaryText: String?
     
     static func determineConfig(from date: Date) -> MonthConfig {
         let monthInt = Calendar.current.component(.month, from: date)
+        let dateInt = Calendar.current.component(.day, from: date)
         
         switch monthInt {
         case 1:
-            return MonthConfig(backgroundColor: .gray,
-                               emojiText: "⛄️",
-                               weekDayTextColor: .black.opacity(0.6),
-                               dayTextColor: .white.opacity((0.8)))
+            if dateInt == 1 {
+                return MonthConfig(backgroundColor: .green,
+                                   emojiText: "🎯",
+                                   weekDayTextColor: .black.opacity(0.6),
+                                   dayTextColor: .white.opacity((0.8)), 
+                                   januaryText: "You did it!")
+            } else {
+                return MonthConfig(backgroundColor: .gray,
+                                   emojiText: "⛄️",
+                                   weekDayTextColor: .black.opacity(0.6),
+                                   dayTextColor: .white.opacity((0.8)), 
+                                   januaryText: nil)
+            }
             
         case 2:
             return MonthConfig(backgroundColor: .palePink,
                                emojiText: "❤️",
                                weekDayTextColor: .black.opacity(0.5),
-                               dayTextColor: .pink.opacity((0.8)))
+                               dayTextColor: .pink.opacity((0.8)), 
+                               januaryText: nil)
             
         case 3:
             return MonthConfig(backgroundColor: .paleGreen,
                                emojiText: "☘️",
                                weekDayTextColor: .black.opacity(0.7),
-                               dayTextColor: .darkGreen.opacity((0.8)))
+                               dayTextColor: .darkGreen.opacity((0.8)),
+                               januaryText: nil)
             
         case 4:
             return MonthConfig(backgroundColor: .paleBlue,
                                emojiText: "☔️",
                                weekDayTextColor: .black.opacity(0.5),
-                               dayTextColor: .purple.opacity((0.8)))
+                               dayTextColor: .purple.opacity((0.8)),
+                               januaryText: nil)
             
         case 5:
             return MonthConfig(backgroundColor: .paleYellow,
                                emojiText: "🌹",
                                weekDayTextColor: .black.opacity(0.5),
-                               dayTextColor: .pink.opacity((0.7)))
+                               dayTextColor: .pink.opacity((0.7)),
+                               januaryText: nil)
             
         case 6:
             return MonthConfig(backgroundColor: .skyBlue,
                                emojiText: "⛅️",
                                weekDayTextColor: .black.opacity(0.5),
-                               dayTextColor: .paleYellow.opacity((0.8)))
+                               dayTextColor: .paleYellow.opacity((0.8)),
+                               januaryText: nil)
             
         case 7:
             return MonthConfig(backgroundColor: .blue,
                                emojiText: "🏖️",
                                weekDayTextColor: .black.opacity(0.5),
-                               dayTextColor: .paleBlue.opacity((0.8)))
+                               dayTextColor: .paleBlue.opacity((0.8)),
+                               januaryText: nil)
             
         case 8:
             return MonthConfig(backgroundColor: .paleOrange,
                                emojiText: "☀️",
                                weekDayTextColor: .black.opacity(0.5),
-                               dayTextColor: .darkOrange.opacity((0.8)))
+                               dayTextColor: .darkOrange.opacity((0.8)),
+                               januaryText: nil)
             
         case 9:           
             return MonthConfig(backgroundColor: .paleRed,
                                emojiText: "🍁",
                                weekDayTextColor: .black.opacity(0.5),
-                               dayTextColor: .paleYellow.opacity((0.9)))
+                               dayTextColor: .paleYellow.opacity((0.9)),
+                               januaryText: nil)
             
         case 10:
             return MonthConfig(backgroundColor: .black,
                                emojiText: "👻",
                                weekDayTextColor: .white.opacity(0.6),
-                               dayTextColor: .orange.opacity((0.8)))
+                               dayTextColor: .orange.opacity((0.8)),
+                               januaryText: nil)
             
         case 11:
             return MonthConfig(backgroundColor: .paleBrown,
                                emojiText: "🦃",
                                weekDayTextColor: .black.opacity(0.6),
-                               dayTextColor: .black.opacity((0.6)))
+                               dayTextColor: .black.opacity((0.6)),
+                               januaryText: nil)
             
             
         case 12:
             return MonthConfig(backgroundColor: .paleRed,
                                emojiText: "🎄",
                                weekDayTextColor: .white.opacity(0.9),
-                               dayTextColor: .darkGreen.opacity((0.7)))
+                               dayTextColor: .darkGreen.opacity((0.7)),
+                               januaryText: nil)
             
         default:
             return MonthConfig(backgroundColor: .gray,
                                emojiText: "📆",
                                weekDayTextColor: .black.opacity(0.6),
-                               dayTextColor: .white.opacity((0.8)))
+                               dayTextColor: .white.opacity((0.8)),
+                               januaryText: nil)
         }
     }
 }
--- Monthly/MonthlyWidget/MonthlyWidget.swift
+++ Monthly/MonthlyWidget/MonthlyWidget.swift
@@ -5,15 +5,15 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> DayEntry {
         DayEntry(date: Date())
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (DayEntry) -> ()) {
         let entry = DayEntry(date: Date())
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [DayEntry] = []
-
+        
         // Generate a timeline consisting of seven entries a day apart, starting from the current date.
         let currentDate = Date()
         
@@ -23,7 +23,7 @@ struct Provider: TimelineProvider {
             let entry = DayEntry(date: startOfDate)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -34,6 +34,9 @@ struct DayEntry: TimelineEntry {
 }
 
 struct MonthlyWidgetEntryView : View {
+    @Environment(\.showsWidgetContainerBackground) var showsBackground
+    //variable for changing UI for widget's night mode
+    @Environment(\.widgetRenderingMode) var renderingMode
     var entry: DayEntry
     var config: MonthConfig
     
@@ -41,70 +44,74 @@ struct MonthlyWidgetEntryView : View {
         self.entry = entry
         self.config = MonthConfig.determineConfig(from: entry.date)
     }
-
+    
     var body: some View {
-        ZStack {
+        VStack {
+            HStack(spacing: 4) {
+                Spacer()
+                 Text(config.emojiText)
+                    .font(.title2)
+                Text(entry.date.weekdayDisplayFormat)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .minimumScaleFactor(0.6)
+                    .foregroundStyle(showsBackground ? config.weekDayTextColor : .white)
+                    .padding(.top, config.januaryText != nil ? 5 : 0)
+                Spacer()
+            }
+            //animate VStack
+            .id(entry.date)
+            .transition(.push(from: .trailing))
+            .animation(.bouncy, value: entry.date)
+            
+            Text(entry.date.dayDisplayFormat)
+                .font(.system(size: 80, weight: .heavy))
+                .foregroundStyle(showsBackground ? config.dayTextColor : .white)
+            //animate number change
+                .contentTransition(.numericText())
+            Text(config.januaryText ?? "")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .id(config.januaryText)
+                .transition(.push(from: .leading))
+                .animation(.spring, value: Text(config.januaryText ?? ""))
+        }
+        .containerBackground(for: .widget) {
             ContainerRelativeShape()
                 .fill(config.backgroundColor.gradient)
-                .frame(maxWidth:.infinity , maxHeight: .infinity )
-            
-            VStack {
-                HStack(spacing: 4) {
-                    Text(config.emojiText)
-                        .font(.title2)
-                    Text(entry.date.weekdayDisplayFormat)
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .minimumScaleFactor(0.6)
-                        .foregroundStyle(config.weekDayTextColor)
-                    Spacer()
-                }
-                
-                Text(entry.date.dayDisplayFormat)
-                    .font(.system(size: 80, weight: .heavy))
-                    .foregroundStyle(config.dayTextColor)
-            }
-            .padding()
         }
-        
     }
 }
 
 struct MonthlyWidget: Widget {
     let kind: String = "MonthlyWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
                 MonthlyWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
             } else {
                 MonthlyWidgetEntryView(entry: entry)
                     .padding()
                     .background()
             }
         }
-        .contentMarginsDisabled()
         .configurationDisplayName("Montly Styled Widget")
         .description("The theme of the widget changes based on month.")
         .supportedFamilies([.systemSmall])
+        //if you don't want to show widget in a particlular mode
+        //.disfavoredLocations([.standBy], for: [.systemSmall])
     }
 }
 
 #Preview(as: .systemSmall) {
     MonthlyWidget()
 } timeline: {
-    DayEntry(date: dateToDisplay(month: 5, day: 10))
-}
-
-//helper fmethod to test different dates
-func dateToDisplay(month: Int, day: Int) -> Date {
-    let components = DateComponents(calendar: Calendar.current,
-                                    year: 2023,
-                                    month: month,
-                                    day: day)
-    
-    return Calendar.current.date(from: components)!
+    MockData.dayOne
+    MockData.dayTwo
+    MockData.dayThree
+    MockData.januaryFirst
 }
 
 extension Date {
@@ -114,5 +121,22 @@ extension Date {
     
     var dayDisplayFormat: String {
         self.formatted(.dateTime.day())
+    }
+}
+
+struct MockData {
+    static let dayOne = DayEntry(date: dateToDisplay(month: 11, day: 4))
+    static let dayTwo = DayEntry(date: dateToDisplay(month: 11, day: 5))
+    static let dayThree = DayEntry(date: dateToDisplay(month: 11, day: 6))
+    static let januaryFirst = DayEntry(date: dateToDisplay(month: 1, day: 1))
+    
+    //helper method to test different widget styles according to dates
+    static func dateToDisplay(month: Int, day: Int) -> Date {
+        let components = DateComponents(calendar: Calendar.current,
+                                        year: 2023,
+                                        month: month,
+                                        day: day)
+        
+        return Calendar.current.date(from: components)!
     }
 }

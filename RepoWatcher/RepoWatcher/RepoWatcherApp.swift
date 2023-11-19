8e8edd54aecf0c2397e42c3d33d3d3c5402ad12b
artexhibit <vendabal@yandex.ru>
2023-11-19 15:35:51 +0300

    init RepoWatcher project, add gitignore

---
+++ RepoWatcher/RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Igor Volkov on 19.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RepoWatcher/RepoWatcherWidget/RepoWatcherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RepoWatcherWidgetBundle.swift
+//  RepoWatcherWidget
+//
+//  Created by Igor Volkov on 19.11.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RepoWatcherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RepoWatcherWidget()
+    }
+}

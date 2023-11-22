dbb027186356386789f1c4f84a37e3cd7c722fbd
artexhibit <vendabal@yandex.ru>
2023-11-22 20:55:20 +0300

    setup UI for RepoWatcher, init UserDefaults to store data

--- RepoWatcher/RepoWatcher/ContentView.swift
+++ RepoWatcher/RepoWatcher/ContentView.swift
@@ -1,14 +1,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newRepo = ""
+    @State private var repos: [String] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("Ex. igorvolkov/Kursvalut", text: $newRepo)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button {
+                        if !repos.contains(newRepo) && !newRepo.isEmpty {
+                            repos.append(newRepo)
+                            UserDefaults.shared.set(repos, forKey: UserDefaults.repoKey)
+                            newRepo = ""
+                        } else {
+                            print("😰 repo already exists or name is empty")
+                        }
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundStyle(.green)
+                    }
+                }
+                .padding()
+                
+                VStack(alignment: .leading) {
+                    Text("Saved Repos")
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                        .padding(.leading)
+                    
+                    List(repos, id: \.self) { repo in
+                        Text(repo)
+                            .swipeActions {
+                                Button("Delete") {
+                                    if repos.count > 1 {
+                                        repos.removeAll{ $0 == repo }
+                                        UserDefaults.shared.set(repos, forKey: UserDefaults.repoKey)
+                                    } else {
+                                        print("😰 there should be at least one repo!")
+                                    }
+                                }
+                                .tint(.red)
+                            }
+                    }
+                }
+            }
+            .navigationTitle("Repo List")
+            .onAppear {
+                guard let retrievedRepos = UserDefaults.shared.value(forKey: UserDefaults.repoKey) as? [String] else {
+                    let defaultValues = ["artexhibit/Kursvalut"]
+                    UserDefaults.shared.setValue(defaultValues, forKey: UserDefaults.repoKey)
+                    repos = defaultValues
+                    return
+                }
+                repos = retrievedRepos
+            }
         }
-        .padding()
     }
 }
 
---
+++ RepoWatcher/RepoWatcher/UserDefaults+Ext.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension UserDefaults {
+    static var shared: UserDefaults {
+        UserDefaults(suiteName: "group.ru.igorcodes.RepoWatcher")!
+    }
+    
+    static let repoKey = "repos"
+}
